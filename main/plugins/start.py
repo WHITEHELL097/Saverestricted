ca3546a4bd7f2ed543d78a3a27c39b12770784b0
WHITEHELL097 <101397767+WHITEHELL097@users.noreply.github.com>
2023-08-23 11:35:27 +0530

    start.py

---
+++ main/plugins/start.py
@@ -0,0 +1,50 @@
+#Github.com/whitehell097
+
+import os
+from .. import bot as Devil
+from telethon import events, Button
+
+from ethon.mystarts import start_srb
+    
+S = '/' + 's' + 't' + 'a' + 'r' + 't'
+
+@Devil.on(events.callbackquery.CallbackQuery(data="set"))
+async def sett(event):    
+    Devil = event.client                    
+    button = await event.get_message()
+    msg = await button.get_reply_message() 
+    await event.delete()
+    async with Devil.conversation(event.chat_id) as conv: 
+        xx = await conv.send_message("Send me any image for thumbnail as a `reply` to this message.")
+        x = await conv.get_reply()
+        if not x.media:
+            xx.edit("No media found.")
+        mime = x.file.mime_type
+        if not 'png' in mime:
+            if not 'jpg' in mime:
+                if not 'jpeg' in mime:
+                    return await xx.edit("No image found.")
+        await xx.delete()
+        t = await event.client.send_message(event.chat_id, 'Trying.')
+        path = await event.client.download_media(x.media)
+        if os.path.exists(f'{event.sender_id}.jpg'):
+            os.remove(f'{event.sender_id}.jpg')
+        os.rename(path, f'./{event.sender_id}.jpg')
+        await t.edit("Temporary thumbnail saved!")
+        
+@Devil.on(events.callbackquery.CallbackQuery(data="rem"))
+async def remt(event):  
+    Devil = event.client            
+    await event.edit('Trying.')
+    try:
+        os.remove(f'{event.sender_id}.jpg')
+        await event.edit('Removed!')
+    except Exception:
+        await event.edit("No thumbnail saved.")                        
+  
+@Devil.on(events.NewMessage(incoming=True, pattern=f"{S}"))
+async def start(event):
+    text = "Send me Link of any message to clone it here, For private channel message, send invite link first.\n\n**SUPPORT:** @devilz_support"
+    await start_srb(event, text)
+
+  
